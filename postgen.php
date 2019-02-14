0d1bcf17cbbb685dc6c7e5156acb1379ee081f7a
Alex P <alex.pantec@gmail.com>
2019-02-14 22:30:20 +0300

    loop

--- postgen.php
+++ postgen.php
@@ -1,11 +1,13 @@
 <?php
 
-require 'utils.php';
-
 use PostGen\User;
 
 require 'vendor/autoload.php';
-require_once 'User.php';
+require 'utils.php';
+require 'User.php';
+
+const THREADS_COUNT = 100;
+const MAX_REPLIES_COUNT = 21;
 
 /** @var User[] $users */
 $users = [
@@ -14,12 +16,15 @@ $users = [
 	new User('user2', 'pass123')
 ];
 
-foreach ($users as $user)
-{
+foreach ($users as $user) {
 	$user->login();
 }
 
 $forum_ids = array_diff(range(6, 35), [23]);
 
-$postId = rand_value($users)->generateThread(rand_value($forum_ids));
-rand_value($users)->generateReply($postId);
+for ($i = 0; $i < THREADS_COUNT; ++$i) {
+	$postId = rand_value($users)->generateThread(rand_value($forum_ids));
+	for ($j = rand(0, MAX_REPLIES_COUNT) - 1; $j >= 0; --$j) {
+		rand_value($users)->generateReply($postId);
+	}
+}
